5ce3d60d52ff6290381d85e7162a7019d7594971
Patrick Schemel <Patrick@schemel.dk>
2022-04-05 13:15:56 +0200

    Initial commit

---
+++ src/com/company/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class BinarySearchTree {
+
+    
+
+}
+
+class Node{
+
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+
+}
